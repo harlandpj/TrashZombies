295b73e7969ada410213fa8af9924ef87d8ce98d
philH <64145845+harlandpj@users.noreply.github.com>
2023-04-11 13:15:07 +0100

    Basic terrain with AJ character from Mixamo
    Basic terrain with part road layout, castle bridge, and river channel. and a few warehouse buildings.
    
    Updated gitignore file to prevent inclusion of "TerrainSampleAssets" and any other used assets from the Asset Store, BUT to include ALL meta files (whether or not they are needed) - I think this is correct!

---
+++ Assets/Scripts/TrashZombies/MyThirdPersonController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using StarterAssets; // for ThirdPersonController
+
+// Class derived from Unity Third Person controller to implement custom events
+// and behaviours for this game
+//
+public class MyThirdPersonController : ThirdPersonController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
